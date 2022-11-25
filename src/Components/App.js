dfaa3daa571b52bd0e876877cef1dc5ca5d322f3
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-11-25 14:55:21 +0100

    add router test navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,28 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./5-Pages/Home"
-import Games from "./5-Pages/Games/Games"
-import ComingSoon from "./5-Pages/Games/ComingSoon"
+import { NavLink, Route, Routes } from "react-router-dom";
+import Home from "./5-Pages/Home";
+import Games from "./5-Pages/Games/Games";
+import ComingSoon from "./5-Pages/Games/ComingSoon";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<Home />} />
-        <Route path="games">
-          <Route index element={<Games />} />
-          <Route path="coming-soon" element={<ComingSoon />} />
+    <>
+      <ul>
+        <li><NavLink end to={"/"}>Home</NavLink></li>
+        <ul>
+          <li><NavLink end to={"/games"}>Games</NavLink></li>
+          <li><NavLink end to={"/games/coming-soon"}>Coming Soon</NavLink></li>
+        </ul>
+      </ul>
+
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="games">
+            <Route index element={<Games />} />
+            <Route path="coming-soon" element={<ComingSoon />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
-  )
+      </Routes>
+    </>
+  );
 }
