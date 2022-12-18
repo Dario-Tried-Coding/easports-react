1320defadbe93217f8861674e9cf3a1ce125d175
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-12-18 23:46:35 +0100

    fix: various fix

--- src/Components/1-Atoms/Button.js
+++ src/Components/1-Atoms/Button.js
@@ -4,7 +4,7 @@ import style from "../../SCSS/1-Atoms/Button.module.scss";
 
 export function Button({marginsBlock = "0", content = "Placeholder text", transparent = true, clr = "var(--clr-dark-700)", bgClr = "transparent", to = ""}) {
     return (
-        <Link to={to}>
+        <Link to={to} style={{display: "inline-block"}}>
             <button style={{ color: `${clr}`, backgroundColor: `${bgClr}`, marginBlock: `${marginsBlock}` }} className={style.btn} type="submit">
                 <span style={{borderColor: `${transparent ? clr : bgClr}`}} className={style.border}></span>
                 {content}
--- src/Components/4-Templates/Section.js
+++ src/Components/4-Templates/Section.js
@@ -1,13 +1,14 @@
 import React from "react";
 import style from "../../SCSS/4-Templates/Section.module.scss"
 import PropTypes from "prop-types"
-import { Button } from "Components/1-Atoms/Button";
+import { Button } from "../1-Atoms/Button";
 
 function Section({title = "Placeholder Title Default", children }) {
   return (
     <div className={`flow-spacer-section-100 ${style.section}`}>
       <h2>{title}</h2>
       {children}
+      <Button />
     </div>
   );
 }
