e2eb3b35f1d1c05466d01a1964c874908ccbc497
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-12-27 09:53:51 +0100

    fix: add default 'data' parameter to Card2Set component

--- src/Components/3-Organisms/Cards2Set.js
+++ src/Components/3-Organisms/Cards2Set.js
@@ -4,7 +4,34 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import WrCards from "./Wrappers/WrCards";
 
-function Cards2Set({ data }) {
+const mockData = [
+  {
+    setTitle: "Set 1",
+    cards: [{}, {}, {}, {}, {}, {}],
+  },
+  {
+    setTitle: "Set 2",
+    cards: [{}, {}, {}, {}, {}, {}],
+  },
+  {
+    setTitle: "Set 3",
+    cards: [{}, {}, {}, {}, {}, {}],
+  },
+  {
+    setTitle: "Set 4",
+    cards: [{}, {}, {}, {}, {}, {}],
+  },
+  {
+    setTitle: "Set 5",
+    cards: [{}, {}, {}, {}, {}, {}],
+  },
+  {
+    setTitle: "Set 6",
+    cards: [{}, {}, {}, {}, {}, {}],
+  },
+];
+
+function Cards2Set({ data = mockData }) {
   const [activeSet, setActiveSet] = useState({ setTitle: "", cards: [] });
 
   useEffect(() => {
@@ -17,7 +44,7 @@ function Cards2Set({ data }) {
 
   return (
     <>
-      <Slider1 data={data} activate={handleClick} activeSet={activeSet}/>
+      <Slider1 data={data} activate={handleClick} activeSet={activeSet} />
       <WrCards gap="--gap-cards-2">
         {activeSet.cards.map((obj, index) => (
           <Card2 key={index} img={obj.img} from={obj.from} date={obj.date} title={obj.title} body={obj.body} />
