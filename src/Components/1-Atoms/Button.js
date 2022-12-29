21fd385987c3b300d59130ccad567e855a9be202
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-12-29 10:01:07 +0100

    feat: add 'fullWidth' prop to 'Button' component

--- src/Components/1-Atoms/Button.js
+++ src/Components/1-Atoms/Button.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import style from "../../SCSS/1-Atoms/Button.module.scss";
 
-export function Button({marginsBlock = "0", content = "Placeholder text", transparent = true, clr = "var(--clr-dark-700)", bgClr = "transparent", to = ""}) {
+export function Button({marginsBlock = "0", fullWidth = false, content = "Placeholder text", transparent = true, clr = "var(--clr-dark-700)", bgClr = "transparent", to = ""}) {
     return (
-        <Link to={to} className={style.link} >
+        <Link to={to} className={fullWidth ? `${style.link} ${style.full}` : `${style.link}`} >
             <button style={{ color: `${clr}`, backgroundColor: `${bgClr}`, marginBlock: `${marginsBlock}` }} className={style.btn} type="submit">
                 <span style={{borderColor: `${transparent ? clr : bgClr}`}} className={style.border}></span>
                 {content}
