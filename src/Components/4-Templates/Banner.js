5b500975d16f6ecf888fdfd2a412df2ae82eea72
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2022-12-30 23:20:03 +0100

    fix: 'Banner' component / fix 'logoImg' positioning based on 'textAlign' prop

--- src/Components/4-Templates/Banner.js
+++ src/Components/4-Templates/Banner.js
@@ -3,6 +3,12 @@ import React from "react";
 import style from "../../SCSS/4-Templates/Banner.module.scss";
 
 export function Banner({ stacked = false, textClrLight=true,logoImg="",title="",subtitle="",body="", margInlineXL = "var(--marg-inline-section)", bgImageData, textPosition = "center", textAlign = "center", btnContent = "default", btnTransparent = true, btnColor = "var(--clr-dark-700)", btnBgColor = "transparent" }) {
+  let logoImgCenteringStyle
+
+  if (textAlign === "left") logoImgCenteringStyle = {marginInline: "0 auto"}
+  else if (textAlign === "center") logoImgCenteringStyle = {marginInline: "auto"}
+  else if (textAlign === "right") logoImgCenteringStyle = {marginInline: "auto 0"}
+  
   return (
     <div className={stacked ? `${style.container} ${style.stacked}` : `${style.container}`}>
       <picture>
@@ -17,7 +23,7 @@ export function Banner({ stacked = false, textClrLight=true,logoImg="",title="",
        
         {/* @ts-ignore */}
         <div className={style.childContent} style={{ textAlign: `${textAlign}`, color:`${textClrLight ? "var(--clr-light-400)" : "var(--clr-dark-700)"}` }}>
-        {logoImg && <img src={logoImg}/> }
+        {logoImg && <img style={logoImgCenteringStyle} src={logoImg}/> }
         {title && <h1 className={style.title} style={{fontSize:"var(--fs-1000)"}}>{title}</h1>}
         {subtitle && <h2 className={style.subtitle} style={{fontSize:"var(--fs-600)"}}>{subtitle}</h2>}
         {body && <h6 className={style.body} style={{fontSize:"var(--fs-500)"}}>{body}</h6>}
