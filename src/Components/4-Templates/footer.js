ac1fa16c03bd6174bb692d32cc7349f9be1a4288
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-02 15:15:01 +0100

    feat:footer began

--- src/Components/4-Templates/footer.js
+++ src/Components/4-Templates/footer.js
@@ -1,7 +1,33 @@
 import React from "react";
-
-const footer = () => {
-  return <div>footer</div>;
+import "../../SCSS/4-Templates/Footer-style.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <section>
+        <div className="section1">
+          <div className="logo">
+            <img src="../../Assets/ea-LOGO.png" alt="logo" />
+          </div>
+          <div className="libreria-giochi">Libreria dei giochi</div>
+          <div className="registrati">Registrati</div>
+          <div className="riscatta-codice">Riscatta codice</div>
+          <div className="EA-app">EA app</div>
+          <div className="informazioni">Informazioni</div>
+          <div className="accessibilità">Accessibilità</div>
+          <div className="aiuto">Aiuto</div>
+        </div>
+        <div className="section2">
+          <div className="section2-n1">Note legali e privacy</div>
+          <div className="section2-n2">Accordo con l'utente</div>
+          <div className="section2-n3">Politica sulla privacy e sui cookie (i tuoi diritti sulla privacy)</div>
+          <div className="section2-n4">Termini di servizio di YouTube</div>
+          <div className="section2-n5">Norme sulla privacy di Google</div>
+          <div className="section2-n6">© 2023 Electronic Arts Inc.</div>
+        </div>
+      </section>
+      <aside></aside>
+    </div>
+  );
 };
 
-export default footer;
+export default Footer;
