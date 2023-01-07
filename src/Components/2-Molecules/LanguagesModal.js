477496cb12ac474915f643e743c15c25a790bf17
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-07 15:05:00 +0100

    flags in languages modal added

--- src/Components/2-Molecules/LanguagesModal.js
+++ src/Components/2-Molecules/LanguagesModal.js
@@ -5,31 +5,154 @@ const LanguagesModal = () => {
     <div className="languagesBlock">
       <div className="firstBlock">
         <ul>
-          <li>United States</li>
-          <li>Norge</li>
-          <li>United Kingdom</li>
-          <li>Spain</li>
-          <li>Sweden</li>
-          <li>Australia</li>
-          <li>Czech Republic</li>
-          <li>Japan</li>
-          <li>France</li>
-          <li>Canada(fr)</li>
-          <li>Korea</li>
-          <li>Germany</li>
-          <li>Canada (en)</li>
-          <li>Italy</li>
-          <li>Denmark</li>
-          <li>Turkey</li>
-          <li>Suomi</li>
-          <li>Polony</li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/usa-flag.jpeg" alt="usa flag" />
+              <p> United States</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/russian-flag-russian-flag-russia-flag-of-russia.jpeg" alt="russian flag" />
+              <p>Русский</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/norway-flag.webp" alt="norway flag" />
+              <p>Norge</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/Flag-United-Kingom.webp" alt="UK flag" />
+              <p>United Kingdom</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/spain-flag.png" alt="spain flag" />
+              <p>España</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/Sweden-Flag.png" alt="sweden flag" />
+              <p>Sverige</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/australia-flag.jpeg" alt="australia flag" />
+              <p>Australia</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/czek-republc.png" alt="czek republic flag" />
+              <p> Česká republika</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/china-flag.webp" alt="china flag" />
+              <p> 中国</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/france-flag.webp" alt="france flag" />
+              <p> France</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/canada-flag.webp" alt="canada flag" />
+              <p> Canada (En)</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/korea-flag.jpeg" alt="korean flag" />
+              <p> 대한민국</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/germany-flag.png" alt="germany flag" />
+              <p>Deutschland</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/canada-flag.webp" alt="canada flag" />
+              <p>Canada (Fr )</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/china-flag.webp" alt="china flag" />
+              <p>繁體中文</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/italy-flag.png" alt="italian flag" />
+              <p>Italia</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/denmark-flag.png" alt="denmark flag" />
+              <p>Denmark</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/turkey-flag.png" alt="turkey flag" />
+              <p>Türkiye</p>
+            </div>
+          </li>
         </ul>
       </div>
       <div className="secondBlock">
         <ul>
-          <li>Mexico</li>
-          <li>Brazil</li>
-          <li>Nederland</li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/japan-flag.png" alt="japan flag" />
+              <p> 日本</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/finland-flag.png" alt="finland flag" />
+              <p> Suomi</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/poland-flag.png" alt="poland flag" />
+              <p> Polska</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/mexico-flag.webp" alt="mexico flag" />
+              <p> México</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/brasil-flag.png" alt="mexico flag" />
+              <p>Brasil</p>
+            </div>
+          </li>
+          <li>
+            <div className="flags">
+              <img className="flagsImg" src="/Assets/netherland-flag.png" alt="netherland flag" />
+              <p>Nederland</p>
+            </div>
+          </li>
         </ul>
       </div>
     </div>
