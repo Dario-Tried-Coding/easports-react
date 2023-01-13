bf0de173983574ecd8f93bc985f3edbd3746c19c
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-01-13 15:02:12 +0100

    feat - 'NavigationNav: add hover functionaly to toggle

--- src/Components/4-Templates/NavigationNav.js
+++ src/Components/4-Templates/NavigationNav.js
@@ -3,6 +3,7 @@ import useWindowDimensions from "Hooks/useWindowDimensions";
 
 import style from "../../SCSS/4-Templates/NavigationNav.module.scss";
 import NavbarDropdown from "Components/1-Atoms/NavbarDropdown";
+import { motion } from "framer-motion";
 
 const mockData = [
   {
@@ -41,26 +42,37 @@ const mockData = [
   },
 ];
 
+const toggleVariants = {
+  hover: { transform: "scale(1.3)", height: "35%", fill: "var(--clr-brand-400)" },
+  rest: { transform: "scale(1)", height: "35%", fill: "var(--clr-dark-900)" }
+}
+const toggleVariants1 = {
+  hover: {fill: "var(--clr-brand-400)"},
+  rest: {fill: "var(--clr-dark-900)"}
+}
+
 function NavigationNav({ data = mockData }) {
   const { width } = useWindowDimensions();
 
   return (
     <div className={style.navigationNav}>
-      <div className={style.toggle}>
-        {width > 1024 ? (
-          <svg viewBox="0 0 16 16" style={{ transform: "rotate(90deg)", height: "35%" }}>
+      {width > 1024 ? (
+        <motion.div className={style.toggle} style={{transform: "rotate(90deg)"}} initial="rest" whileHover="hover">
+          <motion.svg viewBox="0 0 16 16" variants={toggleVariants}>
             <g>
               <circle cx="14.143" cy="8" r="1.857"></circle>
               <circle cx="7.996" cy="8" r="1.857"></circle>
               <circle cx="1.857" cy="8" r="1.857"></circle>
             </g>
-          </svg>
-        ) : (
-          <svg x="0px" y="0px" viewBox="0 0 20 16" style={{ height: "32%" }}>
+          </motion.svg>
+        </motion.div>
+      ) : (
+        <motion.div className={style.toggle} initial="rest" whileHover="hover">
+          <motion.svg x="0px" y="0px" viewBox="0 0 20 16" style={{height: "32%"}} variants={toggleVariants1}>
             <path d="M1,2h18c0.6,0,1-0.4,1-1s-0.4-1-1-1H1C0.4,0,0,0.4,0,1S0.4,2,1,2z M19,7H1C0.4,7,0,7.4,0,8s0.4,1,1,1h18 c0.6,0,1-0.4,1-1S19.6,7,19,7z M19,14H1c-0.6,0-1,0.4-1,1s0.4,1,1,1h18c0.6,0,1-0.4,1-1S19.6,14,19,14z"></path>
-          </svg>
-        )}
-      </div>
+          </motion.svg>
+        </motion.div>
+      )}
 
       <div className={style.logo}>
         <img src="https://media.contentapi.ea.com/content/dam/eacom/common/ea-wordmark-network-nav-coral.svg" alt="logo" />
