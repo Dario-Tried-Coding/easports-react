c5c229ad383d2b6914476fe4ebbb650d160f2c29
Giorgia Russo <giorgiac.russo95@gmail.com>
2023-01-18 22:35:05 +0100

    eseguita card 3 e inserita in home da Roberta e Giorgia, da sistemare con componente wrapper fatto da dario.

--- src/Components/2-Molecules/Cards/Card3.js
+++ src/Components/2-Molecules/Cards/Card3.js
@@ -1,47 +1,55 @@
 import { Button } from "Components/1-Atoms/Button";
 import React from "react";
-import style from "../../../SCSS/2-Molecules/Cards/Card3.module.scss";
+import style from "../../../SCSS/2-Molecules/Cards/Card3.module.scss"
 
 export function Card3(props){
     const {containerClass,
-          imgPath,
-          altIMg,
-          card3title,
-          card3p,
-          rowp,
-          sub,
-          subData
-          } = props
+        imgPath,
+        altIMg,
+        title,
+        height,
+        width,
+        data,
+        generi,
+piattaforma1,
+piattaforma2,
+piattaforma3,
+linkp1,
+linkp2,
+linkp3,
+linkg} = props
     return(
        <section>
         <div className={style.containerClass}>
-            <div className={style.altIMg}>
-            <img src="https://images-ext-1.discordapp.net/external/TmHyAQ8_7jhYOzMFlnM6oE3BV2RiotTttOd-pZQYgnY/https/media.contentapi.ea.com/content/dam/eacom/dead-space/images/2021/07/deadspace-3840x2160.jpg.adapt.crop16x9.1455w.jpg?width=1040&height=585"/>
+            <div>
+            <img src={imgPath}
+              alt={altIMg}
+              style={{height: height, width: width}}/>
             </div>
-            <div className={style.card3title}>Dead Space</div>
+            <div className={style.card3title}>{title}</div>
             <div className={style.card3p}>
                 <div className={style.rowp}>
                     <div className={style.sub}>Data di uscita</div>
-                    <div className={style.subData}>27 gennaio 2023</div>
-                </div>
-                <hr/>
-                <div className={style.rowp}>
-                    <div className={style.sub}>Piattaforme</div>
-                    <div className={style.subData}>
-                     <a href="https://www.ea.com/it-it/games/library/pc-download">PC</a>,
-                     <a href="https://www.ea.com/it-it/games/library/ps5">PlayStation®5</a>,
-                     <a href="https://www.ea.com/it-it/games/library/xbox-series-x">Xbox Series X|S</a>
-                    </div>
-                
+                    <div className={style.subData}>{data}</div>
                 </div>
                 <hr />
                 <div className={style.rowp}>
-                    <div className={style.sub}>Genere:</div>
+                    <div className={style.sub} >Piattaforma</div>
                     <div className={style.subData}>
-                        <a href="https://www.ea.com/it-it/games/library/horror">Horror</a>
+                        <a className={style.a} href={linkp1}>{piattaforma1}</a>,
+                     <a className={style.a} href={linkp2}>{piattaforma2}</a>,
+                     <a className={style.a} href={linkp3}>{piattaforma3}</a>
+                    </div>
+                    
+                </div>
+                <hr />
+                <div className={style.rowp}>
+                    <div className={style.sub}>Genere</div>
+                    <div className={style.subData}>
+                        <a className={style.a} href={linkg}>{generi}</a>
                     </div>
                 </div>
-                <hr className={style.prova}/>
+                <hr />
             </div>
             <div className={style.btn}>
                 <Button />
