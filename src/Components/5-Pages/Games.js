8a9f59740ac2f43aef045802df7c07c045e5e359
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-01-18 22:35:29 +0100

    feat: initialize 'Games' page component

--- src/Components/5-Pages/Games.js
+++ src/Components/5-Pages/Games.js
@@ -1,8 +1,14 @@
 import Navigation from 'Components/4-Templates/Navigation';
 import React from 'react';
+import NavigationData from 'Mock-Data/Games/Navigation';
+import Sidebar from 'Mock-Data/Games/Sidebar';
+import { Links } from 'Components/4-Templates/Links';
 
 export default function Games() {
-  return <Navigation>
-    
+  const navigationData = JSON.parse(NavigationData)
+  const sidebarData = JSON.parse(Sidebar)
+
+  return <Navigation navigationData={navigationData} sidebarData={sidebarData}>
+    <Links />
   </Navigation>
 }
