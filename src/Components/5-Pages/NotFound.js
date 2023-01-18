1d4f1417af075a8d60e401f7b0aa56d695698379
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-01-18 22:29:38 +0100

    feat: add paths to links in 'Navigation' mockdata

---
+++ src/Components/5-Pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function NotFound() {
+  const navigate = useNavigate()
+  setTimeout(() => navigate("/"), 1000)
+
+  return ( <h1>Not Found</h1> );
+}
+
+export default NotFound;
--- src/Mock-Data/Home/Navigation.js
+++ src/Mock-Data/Home/Navigation.js
@@ -5,33 +5,33 @@ const mockData = [
     columns: [
       {
         heading: "Esplora i giochi",
-        links: [["Ultimi giochi", "/games"], ["Prossimamente", "/"], ["Giochi gratuiti", "/"], ["EA SPORTS", "/"], ["EA Originals", "/"], ["Libreria dei giochi", "/"]],
+        links: [["Ultimi giochi", "/games"], ["Prossimamente", "/games/coming-soon"], ["Giochi gratuiti", "/games/library/freetoplay"], ["EA SPORTS", "/sports"], ["EA Originals", "/ea-originals"], ["Libreria dei giochi", "/games/library"]],
       },
       {
         heading: "Piattaforme",
-        links: [["PC", "/"], ["PlayStation®5", "/"], ["Xbox Series X", "/"], ["Nintendo Switch™", "/"], ["Cellulari", "/"]],
+        links: [["PC", "/library/pc-download"], ["PlayStation®5", "/games/library/ps5"], ["Xbox Series X", "/games/library/xbox-series-x"], ["Nintendo Switch™", "/games/library/nintendo-switch"], ["Cellulari", "/games/library/mobile"]],
       },
     ],
   },
   {
     btn: "Altre Esperienze",
     singleColumn: true,
-    links: [["EA Play", "/"], ["L'EA app", "/"], ["Competizioni videoludiche", "/"], ["Diretta di EA Play", "/"], ["Test di gioco", "/"]]
+    links: [["EA Play", "/ea-play"], ["L'EA app", "/ea-app"], ["Competizioni videoludiche", "/"], ["Diretta di EA Play", "/ea-play-live"], ["Test di gioco", "/"]]
   },
   {
     btn: "Informazioni",
     singleColumn: true,
-    links: [["Società", "/"], ["EA Studi", "/"], ["Lavora con noi", "/"], ["La nostra tecnologia", "/"], ["EA Partners", "/"], ["Notizie", "/"], ["Inside EA", "/"]]
+    links: [["Società", "/about"], ["EA Studi", "/ea-studios"], ["Lavora con noi", "/careers"], ["La nostra tecnologia", "/technology"], ["EA Partners", "/ea-partners"], ["Notizie", "/news"], ["Inside EA", "/inside-ea"]]
   },
   {
     btn: "Impegni",
     singleColumn: true,
-    links: [["I nostri impegni", "/"], ["Gioco positivo", "/"], ["Diversità e inclusione", "/"], ["Impatto sociale", "/"], ["Persone e cultura", "/"], ["Ambiente", "/"]]
+    links: [["I nostri impegni", "/commitments"], ["Gioco positivo", "/commitments/positive-play"], ["Diversità e inclusione", "/commitments/inclusion-and-diversity"], ["Impatto sociale", "/commitments/social-impact"], ["Persone e cultura", "/commitments/people-and-culture"], ["Ambiente", "/commitments/environment"]]
   },
   {
     btn: "Risorse",
     singleColumn: true,
-    links: [["Aiuto", "/"], ["Forum", "/"], ["Filtro famiglia", "/"], ["Accessibilità", "/"], ["Stampa", "/"], ["Investitori", "/"]]
+    links: [["Aiuto", "/"], ["Forum", "/forums"], ["Filtro famiglia", "/commitments/positive-play/parental-controls"], ["Accessibilità", "/"], ["Stampa", "/"], ["Investitori", "/"]]
   },
 ];
 
