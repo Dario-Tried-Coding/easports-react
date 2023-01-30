e3a05d38bb4d0d6b2a82ebf436b0f55af5882c10
Roberta <robi.volpato93@gmail.com>
2023-01-30 14:21:48 +0100

    rifare footer parte1

--- src/Components/4-Templates/footer.js
+++ src/Components/4-Templates/footer.js
@@ -1,5 +1,4 @@
 import LanguagesBtn from "Components/2-Molecules/LanguagesBtn";
-
 import RegionsBtn from "Components/2-Molecules/RegionsBtn";
 import logo from "../../Assets/ea-LOGO.png";
 import React from "react";
@@ -8,11 +7,12 @@ import logosvg from "../../Assets/logo.jpg";
 const Footer = () => {
   return (
     <div className="footer">
-      <section>
+      <div className="logo">
+            <img src={logo} />
+      </div>
+      <div className="meno-logo">
         <div className="section1">
-          <div className="logo">
-            <img src={logo} height="90px" width="20px" alt="logo" />
-          </div>
+        <div className="scritte1">
           <div className="libreria-giochi">Libreria dei giochi</div>
           <div className="registrati">Registrati</div>
           <div className="riscatta-codice">Riscatta codice</div>
@@ -20,29 +20,28 @@ const Footer = () => {
           <div className="informazioni">Informazioni</div>
           <div className="accessibilità">Accessibilità</div>
           <div className="aiuto">Aiuto</div>
-        </div>
-        <div className="section2">
+       </div> 
+       <div className="buttons"> 
+          <div className="button1">ciao</div>
+          <div className="button2">ciaoooo</div>
+       </div>
+      </div>
+      <div className="section2">
+       <div className="scritte2">
           <div className="privacy">Note legali e privacy</div>
           <div className="accordo-con-utente">Accordo con l'utente</div>
           <div className="cookie-policy">Politica sulla privacy e sui cookie (i tuoi diritti sulla privacy)</div>
           <div className="youtube-policy">Termini di servizio di YouTube</div>
           <div className="google-privacy">Norme sulla privacy di Google</div>
           <div className="eletronic-arts">© 2023 Electronic Arts Inc.</div>
-        </div>
-      </section>
-      <aside>
-        <div className="regions">
-          <RegionsBtn />
-        </div>
-        <div className="languages">
-          <LanguagesBtn />
-        </div>
-        <div className="online-services">Aggiornamenti sui servizi online</div>
-        <div className="privacy-logo">
-          <img src={logosvg} alt="logo" />
-        </div>
-      </aside>
-    </div>
+          </div>
+          <div className="logo2">
+          <img src="https://privacy-policy.truste.com/privacy-seal/seal?rid=2593a571-b03b-4951-9a22-c175bd66b351"/>
+          </div>
+          </div>
+          </div>
+        
+      </div>
   );
 };
 
