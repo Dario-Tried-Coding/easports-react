b801228c41e3c15665633bbc9242104d13e6f1f1
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-01-30 15:07:38 +0100

    feat: add 'Video' & 'Section1' components

---
+++ src/Components/4-Templates/Section1.js
@@ -0,0 +1,28 @@
+import React from "react";
+import style from "../../SCSS/4-Templates/Section1.module.scss";
+
+function Section1({ children, title = "", subtitle = "", textAlign = "center", paddInline = false, spacerHeightSmall = 0, spacerHeightMedium = 0, spacerHeightLarge = 0, spacerHeightXlarge = 0 }) {
+  return (
+    <div className={style.container} style={{
+      // @ts-ignore
+      "--spacerHeightSmall": `${spacerHeightSmall}vw`,
+      "--spacerHeightMedium": `${spacerHeightMedium}vw`,
+      "--spacerHeightLarge": `${spacerHeightLarge}vw`,
+      "--spacerHeightXlarge": `${spacerHeightXlarge}vw`,
+    }}>
+      <div className={style.spacer}></div>
+      <section
+        className={style.section}
+        data-text-align={textAlign}
+        data-padding-inline={paddInline}
+      >
+        <h3 data-margin-section>{title}</h3>
+        <h4 data-margin-section>{subtitle}</h4>
+        {children}
+      </section>
+      <div className={style.spacer}></div>
+    </div>
+  );
+}
+
+export default Section1;
