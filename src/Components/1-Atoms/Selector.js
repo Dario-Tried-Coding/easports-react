f8e3592ff74740d8a8d4bd5eafb906cb8589577d
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-02-01 00:29:11 +0100

    feat: add 'Selector' component

---
+++ src/Components/1-Atoms/Selector.js
@@ -0,0 +1,54 @@
+// @ts-nocheck
+import useWindowSize from "Hooks/useWindowSize";
+import React from "react";
+import style from "../../SCSS/1-Atoms/Selector.module.scss";
+
+const mockData = [
+  [["Austria"], ["Belgio"], ["Bulgaria"], ["Croazia"], ["Cipro"], ["Repubblica Ceca"], ["Danimarca"], ["Estonia"], ["Finlandia"]],
+  [["Francia"], ["Germania"], ["Greece"], ["Ungheria"], ["Islanda"], ["Irlanda"], ["Italia", "active"], ["Lettonia"], ["Liechtenstein"]],
+  [["Lituania"], ["Lussemburgo"], ["Malta"], ["Paesi Bassi"], ["Norvegia"], ["Polonia"], ["Portogallo"], ["Romania"], ["Slovenia"]],
+  [["Spagna"], ["Svezia"], ["Svizzera"]],
+];
+
+const Selector = ({ isOpen, handleClick, label = "default", data = mockData }, ref) => {
+  const { width } = useWindowSize();
+
+  return (
+    <div ref={ref} className={style.selector}>
+      <button onClick={handleClick} data-active={isOpen}>
+        <span>{label}</span>
+        <span>
+          {data.find(arr => arr.find(arr => arr[1])).find(arr => arr[1] === "active")[0]}
+          <svg style={{transform: isOpen ? "rotate(180deg)" : "rotate(0deg)"}} viewBox="0 0 16 16">
+            <g>
+              <path d="M7.4,9.7l5.8-4.8c0.4-0.4,1-0.4,1.4,0l0,0c0.4,0.4,0.4,1,0,1.4l-5.8,4.8 c-0.4,0.4-1,0.4-1.4,0l0,0C7,10.7,7,10.1,7.4,9.7z"></path>
+              <path d="M7.2,11.1L1.4,6.3C1,5.9,1,5.3,1.4,4.9l0,0c0.4-0.4,1-0.4,1.4,0l5.8,4.8c0.4,0.4,0.4,1,0,1.4 l0,0C8.3,11.5,7.6,11.5,7.2,11.1z"></path>
+            </g>
+          </svg>
+        </span>
+      </button>
+      {width < 1024 ? (
+        <select name="prova" id="prova">
+          {data.map((arr) => arr.map(arr => {
+          if (arr[1]) return <option selected="selected">{arr[0]}</option>
+          else return <option>{arr[0]}</option>
+          } ))}
+        </select>
+      ) : null}
+      <div style={{ display: isOpen ? "flex" : "none" }} className={style.options}>
+        {data.map((set, i) => (
+          <div key={i} className={style.column}>
+            {set.map((arr, i) => (
+              <div key={i} className={style.item}>
+                <span>{arr[0]}</span>
+                {arr[1] === "active" && (<svg viewBox="0 0 16 16"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M7,13.3L7,13.3c-0.4,0.4-1,0.4-1.4,0L0.9,8.7 c-0.4-0.4-0.4-1,0-1.4l0,0c0.4-0.4,1-0.4,1.4,0L7,11.9C7.4,12.3,7.4,12.9,7,13.3z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M5.7,13.4L5.7,13.4c-0.4-0.4-0.5-1-0.1-1.4l8-9.2 c0.4-0.4,1-0.5,1.4-0.1l0,0c0.4,0.4,0.5,1,0.1,1.4l-8,9.2C6.8,13.7,6.1,13.7,5.7,13.4z"></path></g></svg>)}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default React.forwardRef(Selector);
--- src/Components/4-Templates/footer.js
+++ src/Components/4-Templates/footer.js
@@ -1,8 +1,25 @@
-import React from "react";
+import Selector from "Components/1-Atoms/Selector";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import style from "../../SCSS/4-Templates/Footer.module.scss";
 
 const Footer = () => {
+  const [is1Open, setIs1Open] = useState(false)
+  const [is2Open, setIs2Open] = useState(false)
+
+  const selector1Ref = useRef(null)
+  const selector2Ref = useRef(null)
+
+  function handleClick(e) {
+    if (selector1Ref.current.contains(e.target)) {
+      setIs2Open(false)
+      setIs1Open(!is1Open)
+    } else if (selector2Ref.current.contains(e.target)) {
+      setIs1Open(false)
+      setIs2Open(!is2Open)
+    }
+  }
+
   return (
     <div className={style.footer}>
       <div className={style.logo}>
@@ -21,168 +38,9 @@ const Footer = () => {
             <Link to="/">Aiuto</Link>
           </div>
           <div className={style.buttons}>
-            <div></div>
-            <div></div>
+            <Selector label="Prezzi per regione" handleClick={handleClick} ref={selector1Ref} isOpen={is1Open} />
+            <Selector label="Lingua" handleClick={handleClick} ref={selector2Ref} isOpen={is2Open} />
           </div>
-          {/* <div className="buttons"> 
-            <div className="dropdown-price">
-            <div className="dropdown">
-              <button id="swapArrow" className="button-link">
-                <div>Prezzi per Regione</div>
-                <div id="color-nation">
-                  <p>Switzerland <i id="arrow" className="arrow down"></i></p>
-                  
-                </div>
-              </button>
-              <div className="dropdown-menu information-grid">
-                <div className="dropdown-link">
-                  <a href="#" className="link">Austria</a>
-                  <a href="#" className="link">Belgium</a>
-                  <a href="#" className="link">Bulgaria</a>
-                  <a href="#" className="link">Croatia</a>
-                  <a href="#" className="link">Cyprus</a>
-                  <a href="#" className="link">Czech Republic</a>
-                  <a href="#" className="link">Denamrk</a>
-                  <a href="#" className="link">Estonia</a>
-                  <a href="#" className="link">Finland</a>
-                </div>
-                <div className="dropdown-link">
-                  <a href="#" className="link">France</a>
-                  <a href="#" className="link">Germany</a>
-                  <a href="#" className="link">Greece</a>
-                  <a href="#" className="link">Hungary</a>
-                  <a href="#" className="link">Iceland</a>
-                  <a href="#" className="link">Ireland</a>
-                  <a href="#" className="link">Italy</a>
-                  <a href="#" className="link">Latvia</a>
-                  <a href="#" className="link">Liechtenstein</a>
-                </div>
-                <div className="dropdown-link">
-                  <a href="#" className="link">Lithuania</a>
-                  <a href="#" className="link">Luxembourg</a>
-                  <a href="#" className="link">Malta</a>
-                  <a href="#" className="link">Netherlands</a>
-                  <a href="#" className="link">Norway</a>
-                  <a href="#" className="link">Poland</a>
-                  <a href="#" className="link">Portugal</a>
-                  <a href="#" className="link">Romania</a>
-                  <a href="#" className="link">Slovenia</a>
-                </div>
-                <div className="dropdown-link">
-                  <a href="#" className="link">Spain</a>
-                  <a href="#" className="link">Sweden</a>
-                  <a href="#" className="link">Switzerland</a>
-                </div>
-              </div>
-              <div className="select-price">
-                <select name="nation" id="nation">
-                  <option value="austria.html">Austria</option>
-                  <option value="belgium.html">Belgium</option>
-                  <option value="bulgaria.html">Bulgaria</option>
-                  <option value="croatia.html">Croatia</option>
-                  <option value="cyprus.html">Cyprus</option>
-                  <option value="czech-republic.html">Czech Republic</option>
-                  <option value="denmark.html">Denmark</option>
-                  <option value="estonia.html">Estonia</option>
-                  <option value="finland.html">Finland</option>
-                  <option value="france.html">France</option>
-                  <option value="germany.html">Germany</option>
-                  <option value="greece.html">Greece</option>
-                  <option value="hungary.html">Hungary</option>
-                  <option value="italy.html">Italy</option>
-                  <option value="latvia.html">Latvia</option>
-                  <option value="liechtenstein.html">Liechtenstein</option>
-                  <option value="lituania.html">Lituania</option>
-                  <option value="luxembourg.html">Luxembourg</option>
-                  <option value="malta.html">Malta</option>
-                  <option value="netherlands.html">Netherlands</option>
-                  <option value="norway.html">Norway</option>
-                  <option value="poland.html">Poland</option>
-                  <option value="portugal.html">Portugal</option>
-                  <option value="romania.html">Romania</option>
-                  <option value="slovenia.html">Slovenia</option>
-                  <option value="spain.html">Spain</option>
-                  <option value="sweden.html">Sweden</option>
-                  <option value="switzerland.html">Switzerland</option>
-                </select>
-              </div>
-            </div>
-            </div>
-            <div className="dropdown-language">
-            <div className="dropdown">
-              <button className="button-link">
-                <div>Lingua</div>
-                <div id="color-nation">
-                  <span className="fi fi-it"></span>
-                  <div className="wrapper-text-arrow">
-                    <div>Italia</div>
-                    <div>
-                      <img src="" alt="" />
-                    </div>
-                  </div>
-                </div>
-              </button>
-              <div className="dropdown-menu information-grid">
-                <div className="dropdown-link">
-                  <a href="#" className="link"><span className="fi fi-us"></span>United States</a>
-                  <a href="#" className="link"><span className="fi fi-gb"></span>United Kingdom</a>
-                  <a href="#" className="link"><span className="fi fi-au"></span>Australia</a>
-                  <a href="#" className="link"><span className="fi fi-fr"></span>France</a>
-                  <a href="#" className="link"><span className="fi fi-de"></span>Deutschland</a>
-                  <a href="#" className="link"><span className="fi fi-it"></span>Italia</a>
-                  <a href="#" className="link"><span className="fi fi-jp"></span>日本</a>
-                  <a href="#" className="link"><span className="fi fi-pl"></span>Polska</a>
-                  <a href="#" className="link"><span className="fi fi-br"></span>Brasil</a>
-                </div>
-                <div className="dropdown-link">
-                  <a href="#" className="link"><span className="fi fi-ru"></span>Россия</a>
-                  <a href="#" className="link"><span className="fi fi-es"></span>España</a>
-                  <a href="#" className="link"><span className="fi fi-cz"></span>Česká republika</a>
-                  <a href="#" className="link"><span className="fi fi-ca"></span>Canada (En)</a>
-                  <a href="#" className="link"><span className="fi fi-ca"></span>Canada (fr)</a>
-                  <a href="#" className="link"><span className="fi fi-dk"></span>Danmark</a>
-                  <a href="#" className="link"><span className="fi fi-fi"></span>Suomi</a>
-                  <a href="#" className="link"><span className="fi fi-mx"></span>México</a>
-                  <a href="#" className="link"><span className="fi fi-nl"></span>Nederland</a>
-                </div>
-                <div className="dropdown-link">
-                  <a href="#" className="link"><span className="fi fi-se"></span>Sverige</a>
-                  <a href="#" className="link"><span className="fi fi-xx"></span>中国</a>
-                  <a href="#" className="link"><span className="fi fi-kr"></span>대한민국</a>
-                  <a href="#" className="link"><span className="fi fi-xx"></span>繁體中文</a>
-                  <a href="#" className="link"><span className="fi fi-tr"></span>Türkiye</a>
-                </div>
-              </div>
-              <div className="select-language">
-                <select name="language" id="language">
-                  <option value="us.html">United States</option>
-                  <option value="uk.html">United Kingdom</option>
-                  <option value="au.html">Australia</option>
-                  <option value="fr.html">France</option>
-                  <option value="de.html">Deutschland</option>
-                  <option value="it.html">Italia</option>
-                  <option value="jp.html">日本</option>
-                  <option value="pl.html">Polska</option>
-                  <option value="br.html">Brasil</option>
-                  <option value="ru.html">Россия</option>
-                  <option value="es.html">España</option>
-                  <option value="cz.html">Česká republika</option>
-                  <option value="ca-en.html">Canada (En)</option>
-                  <option value="ca-fr.html">Canada (fr)</option>
-                  <option value="dk.html">Danmark</option>
-                  <option value="fi.html">Suomi</option>
-                  <option value="mx.html">México</option>
-                  <option value="nl.html">Nederland</option>
-                  <option value="se.html">Sverige</option>
-                  <option value="#.html">中国</option>
-                  <option value="kr.html">대한민국</option>
-                  <option value="#.html">繁體中文</option>
-                  <option value="tr.html">Türkiye</option>
-                </select>
-              </div>
-          </div>
-            </div>
-          </div> */}
         </div>
         <div className={style.section2}>
           <div className={style.links}>
