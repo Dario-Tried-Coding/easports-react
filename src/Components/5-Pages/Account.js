5e5c65d4d382117c8c65f53f1fb81e0ff0dcbd58
Dario Corbinelli <corbinelli.dario.cd@gmail.com>
2023-02-01 11:23:48 +0100

    mant: add title change handler to 'Form' & 'Account' components

--- src/Components/5-Pages/Account.js
+++ src/Components/5-Pages/Account.js
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
+import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import style from "../../SCSS/5-Pages/Account.module.scss";
 
@@ -14,22 +15,27 @@ const button2Variants = {
 
 export default function Account({}) {
   return (
-    <div className={style.page}>
-      <div className={style.content}>
-        <img src="https://eaassets-a.akamaihd.net/resource_signin_ea_com/551.0.20230120.211.3ef4f9a/p/statics/juno/img/EALogo-New.svg" alt="Logo image" />
-        <h1>Ciao, Nome!</h1>
-        <ul>
-          <li>Nome:</li>
-          <li>Cognome:</li>
-          <li>E-mail:</li>
-          <li>Password:</li>
-        </ul>
-        <Link to="/" className={style.link}>
-          <motion.button type="submit" whileHover="hover" initial="rest" variants={button2Variants}>
-            Log Out
-          </motion.button>
-        </Link>
+    <>
+      <Helmet>
+        <title>Account - Sito ufficiale EA</title>
+      </Helmet>
+      <div className={style.page}>
+        <div className={style.content}>
+          <img src="https://eaassets-a.akamaihd.net/resource_signin_ea_com/551.0.20230120.211.3ef4f9a/p/statics/juno/img/EALogo-New.svg" alt="Logo image" />
+          <h1>Ciao, Nome!</h1>
+          <ul>
+            <li>Nome:</li>
+            <li>Cognome:</li>
+            <li>E-mail:</li>
+            <li>Password:</li>
+          </ul>
+          <Link to="/" className={style.link}>
+            <motion.button type="submit" whileHover="hover" initial="rest" variants={button2Variants}>
+              Log Out
+            </motion.button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
